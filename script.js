f4a241ccc3b3a5c81fa03a13a0e62ecfbb3e0ded
Isku Esenov <snvislm2021@gmail.com>
2021-12-01 21:50:47 +0600

    all files added

---
+++ script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const navbarMenu = document.querySelector(".navbar-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navbarMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navbarMenu.classList.remove("active");
+}))
+
+
